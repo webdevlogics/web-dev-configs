031aa0ea7b909be6e2667fa9da88f5b2168a4106
Michael S Wong <webdevlogics77@gmail.com>
2023-05-21 13:32:11 -0400

    first commit

---
+++ nvim/lua/plugins/alpha.lua
@@ -0,0 +1,9 @@
+return {
+  -- "goolord/alpha-nvim",
+  -- opts = function (_, opts)
+  --   local logo = [[
+  --
+  --   ]]
+  --   opts.section.header.val = vim.split(logo, "\n", {trimempty = true})
+  -- end,
+}
